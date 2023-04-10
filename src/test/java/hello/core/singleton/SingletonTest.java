853e61f19d3a536557565679953e36a7e0f632ea
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-10 22:44:10 +0900

    싱글톤 컨테이너 (스프링 컨테이너)

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -56,9 +58,9 @@ public class SingletonTest {
 //        new SingletonService();
         // 위와 같이 쓰면 에러 SingletonService() has private access
 
-        // 1. 조회 : 호출할 때마다 객체를 반환하는지 조회
+        // 1. 조회 : 호출할 때마다 같은 객체를 반환하는지 확인
         SingletonService singletonService1 = SingletonService.getInstance();
-        // 2. 조회 : 호출할 때마다 객체를 반환하는지 조회
+        // 2. 조회 : 호출할 때마다 같은 객체를 반환하는지 확인
         SingletonService singletonService2 = SingletonService.getInstance();
 
         // 참조값이 같은 것을 확인
@@ -82,5 +84,33 @@ public class SingletonTest {
          */
 
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+//        -> 스프링 컨테이너로 바꾸자
+        // 스프링 컨테이너 덕분에 고객의 요청이 올 때마다 객체를 생성하는 것이 아니라,
+        // 이미 만들어진 객체를 공유해서 효율적으로 재사용 할 수 있다.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때마다 같은 객체를 반환하는지 확인
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        // 2. 조회 : 호출할 때마다 같은 객체를 반환하는지 확인
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        /** 결과
+         * memberService1 = hello.core.member.MemberServiceImpl@5cdd09b1
+         * memberService2 = hello.core.member.MemberServiceImpl@5cdd09b1
+         */
+
+        // 검증
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
